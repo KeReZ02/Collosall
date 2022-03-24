2794942b1f8c5a71370634964823153b101e175a
KeRez02 <bekbolotovakerez@gmail.com>
2022-03-24 17:48:39 +0600

    project

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$('.slider').slick({
+    centerMode: true,
+    arrows:false,
+    dots: true,
+    centerPadding: '',
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
